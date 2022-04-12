8ff0c27a5a5951c76d4ca490c3bd63036780ed0c
Ishan1998 <63969045+Ishan1998@users.noreply.github.com>
2022-04-12 14:22:41 -0400

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,23 @@
+from cProfile import label
+import string
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+from matplotlib.pyplot import text
+
+root = Tk()
+root.title("Digital Clock")
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text = string)
+    label.after(1000, time)
+
+label = Label(root, font=("ds-digital", 80), background="black" ,foreground="cyan")
+
+label.pack(anchor='center')
+time()
+
+mainloop()
